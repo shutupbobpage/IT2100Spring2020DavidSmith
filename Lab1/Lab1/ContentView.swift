f65381432afae07d51aacb1c2f2f627cf01d5a13
David Smith <43157932+shutupbobpage@users.noreply.github.com>
2020-01-14 20:58:55 -0500

    Created Lab1

---
+++ Lab1/Lab1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab1
+//
+//  Created by David Smith on 1/14/20.
+//  Copyright © 2020 David Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World from David!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
